72444823afd0471c0b12fcee2201b1242685e497
Jack Conradson <jdconrad@Jacks-MacBook-Pro.local>
2012-09-22 13:38:23 -0700

    Modified analysis page so blurb can be updated by admins.

--- js/analysis.js
+++ js/analysis.js
@@ -2,7 +2,8 @@ var Analysis = function()
 {
     this.max_comments = 10;
     this.current_week_btn = null;
-    
+    this.edit_blurb = false;
+
     var that = this;
 
     $('.btn-week').click(function() {
@@ -38,7 +39,39 @@ Analysis.prototype.update_week = function(week_btn) {
 
     $('#wheader').html('Analysis (Week ' + this.current_week_btn.text() + ')');
     $('#blurb').html(data.blurb + '<p>Week: ' + this.current_week_btn.text() + '</p>');
- }
+}
+
+
+Analysis.prototype.modify_blurb = function()
+{
+    var save_btn_html = '<button id="save_blurb" class="btn btn-mini btn-primary save">Save</button>';
+    var cancel_btn_html = '<button id="cancel_blurb" class="btn btn-mini btn-primary cancel"><strong>x</strong></button>';
+    $('#edit_blurb_div').html('<div class="btn-group">' + save_btn_html + cancel_btn_html + '</div>');
+
+    this.blurb = $('#blurb').html();
+    $('#blurb').html('<div style="margin: 0; text-align: center"><textarea id="modify_blurb"'
+		     + 'style="width: 90%">' + this.blurb + '</textarea></div>');
+
+    $('#save_blurb').click(function() {analysis.update_blurb(true)});
+    $('#cancel_blurb').click(function() {analysis.update_blurb(false)});
+}
+
+Analysis.prototype.update_blurb = function(save)
+{
+    if (save) {
+	var blurb_text = $('#modify_blurb').val();
+	$('#blurb').html(blurb_text);
+	//ajax to save text?
+    }
+    else {
+	$('#blurb').html(this.blurb);
+    }
+    
+    delete this.blurb;
+
+    $('#edit_blurb_div').html('<button id="edit_blurb" class="btn btn-mini"><i class="icon-edit"></i></button>');
+    $('#edit_blurb').click(function() {analysis.modify_blurb()});
+}
 
 Analysis.prototype.update_stats = function()
 {
